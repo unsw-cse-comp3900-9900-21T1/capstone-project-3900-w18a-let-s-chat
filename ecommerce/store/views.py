fa5819c9ebbc3b1d42cd359c5ee4652a9b0e2c31
Daniel Gordos <danieljgordos@gmail.com>
2021-03-18 20:16:24 +1100

    Implemented product page view tracking. Updated work diary

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -112,6 +112,8 @@ def product_page(request, slug=None):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		cartItems = order.get_cart_items
+
+		ProductViewCount.log(customer, product)
 	else:
 		#Create empty cart for now for non-logged in user
 		order = {'get_cart_total':0, 'get_cart_items':0}
