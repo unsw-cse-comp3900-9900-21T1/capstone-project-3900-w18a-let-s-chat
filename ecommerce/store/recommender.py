4e745e2b563a7fb748062cee09826bdde44af04d
Daniel Gordos <danieljgordos@gmail.com>
2021-03-20 18:53:27 +1100

    Fixed bug with recommender when user has no viewed pages

--- ecommerce/store/recommender.py
+++ ecommerce/store/recommender.py
@@ -46,6 +46,10 @@ class Recommender():
 
         product_dict = {tag: 1 for tag in product.tags.names()}
 
+        # Return zero similarity if either user or product has no tags (eg. new users)
+        if all([count==0 for count in product_dict.values()]) or all([count==0 for count in self.profile_dict.values()]):
+            return 0.0
+
         # Find cosine similarity between two dicts
         numerator = 0.0
         denom_a = 0.0
