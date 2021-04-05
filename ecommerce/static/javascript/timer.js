b394414e75d0cf33a59884c59d43a2a8889b799f
jeremyyip98 <30544127+jeremyyip98@users.noreply.github.com>
2021-04-05 21:38:24 +1000

    Fixed the timer display error

--- ecommerce/static/javascript/timer.js
+++ ecommerce/static/javascript/timer.js
@@ -1,16 +1,20 @@
-var eventBox = document.getElementById('event-box').textContent
+var eventBox = document.getElementById('event-box').innerHTML
+// var end_date = new Date(eventBox * 1000)
+// console.log("end_date: " + end_date)
+// console.log("eventBox: " + eventBox)
 var countdownBox = document.getElementById('countdown-box')
 
 // console.log(eventBox);
-var eventDate = Date.parse(eventBox)
-// console(eventDate);
+var eventDate = new Date(eventBox * 1000).getTime()
+// var eventDate = end_date.getTime()
+// console.log("eventdate: " + eventDate)
 
 var myCountdown = setInterval(()=>{
     var now = new Date().getTime()
-    //console.log(now)
+    // console.log("now: " + now)
 
     var diff = eventDate - now
-    //console.log(diff)
+    // console.log("diff: " + diff)
     
     var d = Math.floor(eventDate / (1000 * 60 * 60 * 24) - (now / (1000 * 60 * 60 * 24)))
     var h = Math.floor((eventDate / (1000 * 60 * 60) - (now / (1000 * 60 * 60))) % 24)
