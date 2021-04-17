eaaa9fe19280e5d0916547e385b452e49502f404
jeremyyip98 <jeremyyip98@gmail.com>
2021-04-17 20:36:17 +1000

    Fixed error when anonymous user access the store page

--- ecommerce/store/templatetags/support_tags.py
+++ ecommerce/store/templatetags/support_tags.py
@@ -1,8 +1,12 @@
 from django import template
+from ..models import Customer
 
 register = template.Library()
 
 @register.simple_tag
 def check_exist_tag(customer, product):
-    wishlist = customer.wishlist
-    return customer.wishlist.check_exist(wishlist, product)
+    if isinstance(customer, Customer):
+        wishlist = customer.wishlist
+        return customer.wishlist.check_exist(wishlist, product)
+    else:
+        return False
