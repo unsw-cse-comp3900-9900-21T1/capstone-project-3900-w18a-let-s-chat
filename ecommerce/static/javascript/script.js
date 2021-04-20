997051856cdcda5b319e4407b611a219440dda47
ilze3e3 <78832179+ilze3e3@users.noreply.github.com>
2021-04-20 15:03:15 +1000

    Merged from Product Searching fixing migrations file

--- ecommerce/static/javascript/script.js
+++ ecommerce/static/javascript/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (function(d, m){
     var kommunicateSettings = 
         {"appId":"232096f64f61d669fec1cc191fadff44e","popupWidget":true,"automaticChatOpenOnNavigation":true};
@@ -7,3 +8,14 @@
     window.kommunicate = m; m._globals = kommunicateSettings;
 })(document, window.kommunicate || {});
 /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+=======
+(function(d, m){
+    var kommunicateSettings = 
+        {"appId":"232096f64f61d669fec1cc191fadff44e","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+>>>>>>> Product_Searching
--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.contrib import admin
 
 from .models import *
@@ -13,3 +14,18 @@ admin.site.register(ProductReview)
 admin.site.register(ReviewReact)
 admin.site.register(Bidder)
 admin.site.register(Wishlist)
+=======
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+# admin.site.register(Tag)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+admin.site.register(ProductViewCount)
+admin.site.register(ProductReview)
+admin.site.register(ReviewReact)
+>>>>>>> Product_Searching
