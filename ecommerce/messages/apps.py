6f39aeb61212392ef03a3eebb8d8e4bebf50ac67
ilze3e3 <78832179+ilze3e3@users.noreply.github.com>
2021-04-20 14:20:50 +1000

    Got rid of messages inside project folder so it works through a fresh install on the venv

--- ecommerce/messages/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig as BaseAppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class AppConfig(BaseAppConfig):
-
-    name = "pinax.messages"
-    label = "pinax_messages"
-    verbose_name = _("Pinax Messages")
--- ecommerce/messages/tests/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r"^", include("pinax.messages.urls", namespace="pinax_messages")),
-]
