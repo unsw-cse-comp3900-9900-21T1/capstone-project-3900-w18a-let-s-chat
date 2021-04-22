c014bbc36c29b33dc9e7f2f47014eb0d683d56e5
wellson <z5183987@ad.unsw.edu.au>
2021-04-22 16:45:40 +0800

    new kommunicate appID updated

--- ecommerce/static/javascript/script.js
+++ ecommerce/static/javascript/script.js
@@ -7,3 +7,16 @@
     window.kommunicate = m; m._globals = kommunicateSettings;
 })(document, window.kommunicate || {});
 /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+
+
+// New appID : adcce7b6296ec3fcc77de67b1e9ce45d select web you will see the script like this:
+<script type="text/javascript">
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"adcce7b6296ec3fcc77de67b1e9ce45d","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+</script>
